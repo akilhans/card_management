8000e994839370aae2e71e029aed1707492e42be
Okiljon <okiljondadakhonov@gmail.com>
2026-07-06 13:44:44 +0500

    sdaads

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: 'https://card-management-c11w.vercel.app/api',
 });
 
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
