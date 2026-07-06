b10b51428c7cd1fbd4048d3c1bbab75c1dbb10c6
Okiljon <okiljondadakhonov@gmail.com>
2026-07-07 01:47:56 +0500

    sdasd

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://card-management-c11w.vercel.app/api',
+  baseURL: 'easygoing-cat-production-eeff.up.railway.app/api',
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
