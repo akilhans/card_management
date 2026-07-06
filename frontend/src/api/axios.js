bef2723d6da3ed52e5ef46364284201caff508bc
Okiljon <okiljondadakhonov@gmail.com>
2026-07-06 13:43:12 +0500

    sdaads

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://card-management-c11w.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
