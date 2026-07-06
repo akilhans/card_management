d22ec8d41af29fdb877098d3d2313ac1947a8094
Okiljon <okiljondadakhonov@gmail.com>
2026-07-07 02:23:00 +0500

    dssd

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'easygoing-cat-production-eeff.up.railway.app/api',
+  baseURL: 'easygoing-cat-production-eeff.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
